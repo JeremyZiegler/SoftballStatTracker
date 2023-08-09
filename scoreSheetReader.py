c232e25dea5531425e3005c513257fd4eddc2d43
JeremyZiegler <jeremy.ziegler70@gmail.com>
2023-08-09 16:58:56 -0700

    Added initial Python files

---
+++ scoreSheetReader.py
@@ -0,0 +1,8 @@
+import opencv
+
+"""
+This program will use uploaded pictures of a custom softball score sheet to track stats of players.
+The stats to be tracked are: at bats (AB), runs (R), runs batted in (RBI), Walks (BB), Strikeouts (K),
+batting average (AVG), On-base percentage (OBP), Slugging (SLG), On-base plus slugging (OPS), single (1B),
+double (2B), triple (3B), home run (HR), total bases (TB), games played (GP)
+"""
